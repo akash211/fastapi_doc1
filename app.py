0344ab478d1cd40c604f0112ecaae869bba42a1f
Akash Kumar <akash211@gmail.com>
2023-11-02 02:08:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,3 +21,14 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
