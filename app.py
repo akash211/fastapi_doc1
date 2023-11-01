cd2305f33717b7ebfe35151aa45fe64138473b32
Akash Kumar <akash211@gmail.com>
2023-11-02 02:11:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,15 +23,9 @@ async def get_model(model_name: ModelName):
     return {"model_name": model_name, "message": "Have some residuals"}
 
 
-@app.get("/users/{user_id}/items/{item_id}")
+@app.get("/items/{item_id}")
 async def read_user_item(
-    user_id: int, item_id: str, q: str | None = None, short: bool = False
+    item_id: str, needy: str, skip: int = 0, limit: int | None = None
 ):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
     return item
