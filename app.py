4e1312c0a6c4d11c3998ef28bf30f042877b0653
Akash Kumar <akash211@gmail.com>
2023-11-03 13:12:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(
-    q: Annotated[str | None, Query(min_length=3, max_length=50)] = None
-):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
+async def read_items(q: Annotated[list[str] | None, Query()] = None):
+    query_items = {"q": q}
+    return query_items
