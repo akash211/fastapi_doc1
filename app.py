ccafaf165ededcbad503966bfd93ee28afa35aad
Akash Kumar <akash211@gmail.com>
2023-11-03 21:53:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ async def read_items(
     q: Annotated[
         str | None,
         Query(
-            alias="item-query",
+            alias="item-query",  # http://127.0.0.1:8000/items/?item-query=foobaritems
             title="Query string",
             description="Query string for the items to search in the database that have a good match",
             min_length=3,
             max_length=50,
-            pattern="^fixedquery$",
-            deprecated=True,
+            # pattern="^fixedquery$",https://app.snowflake.com/sklzfgu/ne38482/worksheets
+            # deprecated=True,
         ),
     ] = None,
 ):
