055f991e2b303a91a12313eaaa5ec564840b3483
Akash Kumar <akash211@gmail.com>
2023-11-09 01:47:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
-from typing import Annotated
-
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/items/")
-async def read_items(
-    q: Annotated[
-        str | None,
-        Query(
-            alias="item-query",  # http://127.0.0.1:8000/items/?item-query=foobaritems
-            title="Query string",
-            description="Query string for the items to search in the database that have a good match",
-            min_length=3,
-            max_length=50,
-            # pattern="^fixedquery$",https://app.snowflake.com/sklzfgu/ne38482/worksheets
-            # deprecated=True,
-        ),
-    ] = None,
-):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
+class Image(BaseModel):
+    url: str
+    name: str
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: set[str] = set()
+    image: Image | None = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
     return results
